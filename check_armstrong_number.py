9332ef0193c54e92d4f18c0e8e031a16fcdd4e05
Bala Dharshini <baladharshini.a_25it@kgkite.ac.in>
2026-08-21 17:43:45 +0530

    check armstrong numbeer

---
+++ check_armstrong_number.py
@@ -0,0 +1,11 @@
+num=int(input("Enter a number:"))
+temp=num
+sum=0
+while temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
+if sum==num:
+    print("Armstrong number")
+else:
+    print("Not a armstrong number")        
